d680723a5f42327407879f31c579868fe741555d
Ankit Kanojia <ankit.kanojia@devdigital.com>
2021-04-23 18:40:13 +0530

    Update code for server with socket programming

--- server/server.js
+++ server/server.js
@@ -1,68 +1,57 @@
-// const express = require("express");
-// const http = require("http");
-// const socketIo = require("socket.io");
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-// const port = process.env.PORT || 4000;
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server); // < Interesting!
-// const uri = process.env.ATLAS_URI;
-
-// mongoose 
-//  .connect(uri, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useCreateIndex: true,   })   
-//  .then(() => console.log("Database connected!"))
-//  .catch(err => console.log(err));
-
-// io.on('connection', client => {
-//     console.log("New client connected");
-//     client.on('disconnect', () => { });
-// });
-
-// const userRouter = require('./routes/users')(io);
-// const postRouter = require('./routes/posts');	
-
-// app.use('/users', userRouter);
-// app.use('/posts', postRouter);
-
-// server.listen(port, function () {
-//     console.log('listening on *:4000');
-// });
-
-const express = require('express')
-const cors = require('cors');
-const app = express();
-const port = process.env.port || 4000;
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 require('dotenv').config();
-
-const uri = process.env.ATLAS_URI;
-mongoose 
- .connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,   })   
- .then(() => console.log("Database connected!"))
- .catch(err => console.log(err));
-
-app.use(cors());
-app.use(express.json());
+const cors = require('cors');
 
 const userRouter = require('./routes/users');
 const postRouter = require('./routes/posts');	
 const authRouter = require('./routes/auth');	
 
-app.use('/users', userRouter);
-app.use('/posts', postRouter);
-app.use('/auth', authRouter);
+const uri = process.env.ATLAS_URI;
+const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+// Set CORS header
+app.use(cors());
+
+// Error Handler
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data; // Passing original error data
+  res.status(status).json({ message: message, data: data });
+});
+
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
+app.use('/auth', authRouter);
+
+// DB connection
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(result => {
+    const port = process.env.PORT || 4000;
+    const server = app.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+    });
+
+    //https://github.com/EllisMin/mern-demo-production
+		//https://github.com/EllisMin/MERN-demo-production/blob/master/app.js
+    // Create socket io connection
+    const io = require("./socket").init(server);
+    // Connection listeners with client
+    io.on("connection", socket => {
+      console.log("Client connected");
+    });
+  })
+  .catch(err => {
+    // Handle error
+  });
